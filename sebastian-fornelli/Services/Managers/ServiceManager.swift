1791212af69e3235d276c331a63e5ec26570214e
Sebastián Fornelli <sfornel1@corp.vodafone.es>
2022-02-15 21:11:31 +0100

    Add API Manager

--- sebastian-fornelli/Services/Managers/ServiceManager.swift
+++ sebastian-fornelli/Services/Managers/ServiceManager.swift
@@ -10,13 +10,16 @@ import Foundation
 class ServiceManager {
     public static let shared = ServiceManager()
     
-    func callService<T: Decodable>(urlString: String, success: @escaping ((T) -> Void), failure: @escaping (() -> Void)) {
+    func callService<T: Decodable>(urlString: String,
+                                   httpMethod: HttpVerb,
+                                   success: @escaping ((T) -> Void),
+                                   failure: @escaping (() -> Void)) {
         let url = URL(string: urlString)
         guard let urlObj = url else { return }
         let session = URLSession.shared
         var request = URLRequest(url: urlObj)
-        request.httpMethod = "GET"
-        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.httpMethod = httpMethod.rawValue
+        request.addValue("Client-ID \(ManagersConstants.clientID)", forHTTPHeaderField: "Authorization")
         
         let task: URLSessionDataTask = session.dataTask(with: request as URLRequest, completionHandler: {
             data, response, error in
