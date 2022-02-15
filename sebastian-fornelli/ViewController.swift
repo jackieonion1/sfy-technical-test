b565e6d44be64d2593db4c185b3176e19c47f4b3
Sebastián Fornelli <sfornel1@corp.vodafone.es>
2022-02-15 19:59:06 +0100

    Initial Commit

---
+++ sebastian-fornelli/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sebastian-fornelli
+//
+//  Created by Sebastián Fornelli on 15/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
