e2f4808b1af7367ce5742ec4264fc62163dd2ae6
Sebastián Fornelli <sfornel1@corp.vodafone.es>
2022-02-16 10:33:24 +0100

    Add simple collectionView with custom cells

--- sebastian-fornelli/ViewControllers/ViewController.swift
+++ sebastian-fornelli/ViewControllers/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(CollectionViewCell.self, forCellWithReuseIdentifier: "collectionViewCell")
     }
-
-
 }
 
+// MARK: UICollectionViewDelegate
+extension ViewController: UICollectionViewDelegate {
+    
+}
+
+// MARK: UICollectionViewDelegateFlowLayout
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = (view.frame.width - 20) / 3
+        return CGSize(width: width, height: width)
+    }
+}
+
+// MARK: UICollectionViewDataSource
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionViewCell", for: indexPath) as? CollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        cell.configure()
+        return cell
+    }
+}
---
+++ sebastian-fornelli/Views/Cells/CollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CollectionViewCell.swift
+//  sebastian-fornelli
+//
+//  Created by Sebastián Fornelli on 16/2/22.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    func configure() {
+        self.backgroundColor = .blue
+    }
+}
